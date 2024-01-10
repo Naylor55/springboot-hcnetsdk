112ef0670329f49790602e0416594c64a4f7392e
陈 <cnaylor@163.com>
2024-01-10 13:08:02 +0800

    init  project

---
+++ src/main/java/com/ramble/springboothcnetsdk/SpringbootHcnetsdkApplication.java
@@ -0,0 +1,13 @@
+package com.ramble.springboothcnetsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHcnetsdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHcnetsdkApplication.class, args);
+    }
+
+}
